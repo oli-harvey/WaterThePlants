3c7eaaf0ee8203cb809b26564f5e723d61d031c6
Oliver Harvey <oliharvey@hotmail.com>
2020-11-07 18:02:18 +0000

    Initial Commit

---
+++ WaterThePlants/WaterThePlantsApp.swift
@@ -0,0 +1,20 @@
+//
+//  WaterThePlantsApp.swift
+//  WaterThePlants
+//
+//  Created by Oliver Harvey on 07/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WaterThePlantsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
