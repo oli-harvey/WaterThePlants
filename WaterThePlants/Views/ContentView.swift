fbe90f2ecaac7607cfec2bb9518bcd83ee6f6d84
Oliver Harvey <oliharvey@hotmail.com>
2020-12-05 13:40:19 +0000

    Filter by due animations fixed

--- WaterThePlants/Views/ContentView.swift
+++ WaterThePlants/Views/ContentView.swift
@@ -12,7 +12,10 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @State var showingTaskStatus: TaskStatus = .running
     @State var dueWithin: TimePart = .year
+    @State var dueWithinSelection: String = "All"
+    @State var dueFilterOff = false
     var timePartsToSelect: [TimePart] = [.day, .week, .month, .year]
+    var timePartsToSelectWithAll: [String] = [TimePart.day.rawValue, TimePart.week.rawValue, TimePart.month.rawValue, TimePart.year.rawValue, "All"]
     @State var showingTaskDetail = false
     @State var dummy = false
     var moveIn: Edge {
@@ -33,23 +36,34 @@ struct ContentView: View {
                     .padding()
                 }
                     .pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal)
                 if showingTaskStatus == .running {
-                    Picker("Due within", selection: $dueWithin.animation(.easeOut(duration: 0.4))) {
-                        ForEach(timePartsToSelect, id: \.self) { timePart in
-                            Text(timePart.rawValue)
+                    Picker("Due within", selection: $dueWithinSelection.animation(.easeOut(duration: 0.4))) {
+                        ForEach(timePartsToSelectWithAll, id: \.self) { timePart in
+                            Text(timePart)
                         }
                         .padding()
                     }
+                    .onChange(of: dueWithinSelection) { value in
+                        withAnimation(.easeOut(duration: 0.4)) {
+                            dueFilterOff = value == "All"
+                            if value != "All" {
+                                dueWithin = TimePart(rawValue: value) ?? .year
+                            }
+                        }
+                    }
                     .pickerStyle(SegmentedPickerStyle())
+                    .padding(.horizontal)
                 }
                 // results
-                FilteredGrid(filter: showingTaskStatus.rawValue, showingTaskDetail: $showingTaskDetail, dueWithin: dueWithin, dummy: $dummy)
+                FilteredGrid(filter: showingTaskStatus.rawValue, showingTaskDetail: $showingTaskDetail, dueFilterOff: dueFilterOff, dueWithin: dueWithin, dummy: $dummy)
             }
             .padding(.vertical)
             .navigationBarTitle("", displayMode: .inline)
             .toolbar {
                 ToolbarItemGroup(placement: .navigationBarLeading) {
                     VStack {
+                        Text("") //  to seperate header better
                         HStack {
                             Image("Logo")
                                 .resizable()
--- WaterThePlants/Views/FilteredGrid.swift
+++ WaterThePlants/Views/FilteredGrid.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FilteredGrid: View {
     @Environment(\.managedObjectContext) private var viewContext
     @Binding var showingTaskDetail: Bool
+    var dueFilterOff: Bool = false
     var dueWithin: TimePart = .year
 //    var moveIn: Edge
     var fetchRequest: FetchRequest<Task>
@@ -19,7 +20,7 @@ struct FilteredGrid: View {
     let columns = [
         GridItem(.adaptive(minimum: 400))
     ]
-    init(filter: String, showingTaskDetail: Binding<Bool>, dueWithin: TimePart, dummy: Binding<Bool>) {
+    init(filter: String, showingTaskDetail: Binding<Bool>, dueFilterOff: Bool, dueWithin: TimePart, dummy: Binding<Bool>) {
         
         fetchRequest = FetchRequest<Task>(entity: Task.entity(),
                                           sortDescriptors: [NSSortDescriptor(keyPath: \Task.dueDate, ascending: true)],
@@ -28,19 +29,27 @@ struct FilteredGrid: View {
         self._dummy = dummy
         self._showingTaskDetail = showingTaskDetail
         self.dueWithin = dueWithin
+        self.dueFilterOff = dueFilterOff
     }
     
     var body: some View {
         ScrollView {
              LazyVGrid(columns: columns, spacing: 20) {
-                ForEach(tasks.filter {$0.dueWithinTimeParts.contains(dueWithin)}) { task in
+                ForEach(tasksFiltered()) { task in
                      ProgressView(task: task, dummy: $dummy)
                         .padding(.vertical)
                      //   .transition(.move(edge: moveIn))
                         .transition(.scale)
-                 }
+                 }                
              }
          }
+    }
+    func tasksFiltered() -> [Task] {
+        if dueFilterOff {
+            return tasks.compactMap{ $0 }
+        } else {
+            return tasks.filter {$0.dueWithinTimeParts.contains(dueWithin)}
+        }
     }
 }
 
